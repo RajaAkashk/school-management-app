7d737a2cdb56d2ab56162dc0dabc744578a3831b
Raja Akash Kumar <rajaakashk9@gmail.com>
2024-12-20 12:05:45 +0530

    inital Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../src/features/counterSlice";
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
